f3f96f327bb6076d69de4e897692324f73c945c6
xdnishantvit <nishant.tiwari2021@vitbhopal.ac.in>
2024-03-22 16:48:25 +0530

    initial commit

---
+++ Medium/Diagonal sum in binary trees.py
@@ -0,0 +1,125 @@
+"""
+Consider Red lines of slope -1 passing between nodes (in following diagram). The diagonal sum in a binary tree is the sum of all node datas lying between these lines. Given a Binary Tree of size n, print all diagonal sums.
+"""
+
+
+
+
+#User function Template for python3
+
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    #Complete the function below
+    def diagonalSum(self, root):
+        diagonal_sums = {}
+        
+        def dfs(node , diagonal_level):
+            if not node:
+                return
+            
+            diagonal_sums[diagonal_level]= diagonal_sums.get(diagonal_level, 0) + node.data
+            
+            dfs(node.left, diagonal_level + 1)
+            dfs(node.right, diagonal_level)
+    
+        dfs(root, 0)
+        return diagonal_sums.values()
+        #:param root: root of the given tree.
+        
+        #code here
+    
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        res = ob.diagonalSum(root)
+        for i in res:
+            print (i, end = " ")
+        print()
+
+# } Driver Code Ends
