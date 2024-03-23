7b78b3bd595c30a2726f1969c4541c5a3ba67921
xdnishantvit <nishant.tiwari2021@vitbhopal.ac.in>
2024-03-23 12:25:18 +0530

    initial commit

---
+++ Easy/Fibonacci series up to Nth term.py
@@ -0,0 +1,30 @@
+"""
+You are given an integer n, return the fibonacci series till the nth(0-based indexing) term. Since the terms can become very large return the terms modulo 109+7.
+"""
+
+#User function Template for python3
+
+class Solution:
+    def series(self, n):
+        # Code here
+        dp = [0,1]
+        for i in range(2,n+1):
+            x = dp[-1]+dp[-2]
+            dp.append(x%(10**9+7))
+        return dp
+        # Code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        result = ob.series(N)
+        print(*result)
+# } Driver Code Ends
+
